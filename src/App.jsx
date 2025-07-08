e404986aa21a70f553344dc6bc76a0a95e114205
pooja630-ctrl <poojamamankar630@gmail.com>
2025-07-08 10:32:22 +0530

    "initial commit"

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './componant/todo'
 
 function App() {
   return (
     <>
       <h1>Study redux</h1>
+      <Todo />
     </>
   )
 }
